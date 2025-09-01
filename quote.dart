486f85f69075049563f233ad492ae859f8b70e74
noah <47998735+noahrcarter@users.noreply.github.com>
2025-08-31 23:38:23 -0400

    Add files via upload

---
+++ quote.dart
@@ -0,0 +1,27 @@
+class Quote {
+
+  String text;
+  String author;
+  String category;
+  DateTime created;
+
+  //  normal constructor, as we've already seen
+
+  //  Quote(String author, String text){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  //  constructor with named parameters
+
+  //  Quote({ String author, String text }){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  // constructor with named parameters
+  // & automatically assigns named arguments to class properties
+
+  Quote({ required this.text, required this.author, required this.category, required this.created});
+
+}
